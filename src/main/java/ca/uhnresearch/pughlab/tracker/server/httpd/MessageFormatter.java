85f4ed1a64466b6aa0b98832643df31377d3dc0f
denis554 <denisignatenko554@gmail.com>
2015-08-19 11:24:43 -0400

    Added a new formatter to make a request log appear simply.
    That doesn't need any of the Java logging background noise

---
+++ src/main/java/ca/uhnresearch/pughlab/tracker/server/httpd/MessageFormatter.java
@@ -0,0 +1,13 @@
+package ca.uhnresearch.pughlab.tracker.server.httpd;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class MessageFormatter extends Formatter {
+
+	@Override
+	public String format(LogRecord record) {
+		return record.getMessage().concat("\n");
+	}
+
+}
